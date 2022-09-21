32a943c2aad6ba4bf7c1173c3b0e97c4cf89abd2
Ronan Singpurwala <singpurwala13@gmail.com>
2022-09-21 15:18:23 +0100

    a

--- sentiment_analysis_app/urls.py
+++ sentiment_analysis_app/urls.py
@@ -20,5 +20,6 @@ from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index),
+    url(r'^text-analysis/$', views.text_analysis),
     url(r'^yelp/$', views.yelp),
 ]
--- sentiment_analysis_app/views.py
+++ sentiment_analysis_app/views.py
@@ -2,18 +2,22 @@ from django.shortcuts import render
 from .analysis import analysis as a
 
 def index(request):
+    return render(request, "index.html")
+
+
+def text_analysis(request):
     if "submit-button" in request.POST:
         user_input = str(request.POST.get("entered"))
         
         #minimum charcter limit BERT
         if len(user_input) < 3:
-            return render(request, "index.html", {"error": "Please enter atleast 3 characters!"})
+            return render(request, "text_analysis.html", {"error": "Please enter atleast 3 characters!"})
         
         result = a.calculating(user_input)
         sentence = "The sentiment is "
-        return render(request, "index.html",{"result": result, "sentence": sentence})
+        return render(request, "text_analysis.html",{"result": result, "sentence": sentence})
     
-    return render(request, "index.html")
+    return render(request, "text_analysis.html")
 
 
 def yelp(request):
