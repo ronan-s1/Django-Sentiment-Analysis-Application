178a3ce8e6dc1b5ab1697cd711e7915a2f4f3e4f
Ronan Singpurwala <singpurwala13@gmail.com>
2022-09-22 01:08:12 +0100

    a

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -6,3 +6,12 @@ function load()
     document.getElementById("load").style.visibility = "visible";
     document.getElementById("display").style.visibility = "hidden";
 }
+
+function yelp_load()
+{
+    document.getElementById("submit-button").style.display = "none";
+    document.getElementById("load").style.visibility = "visible";
+    document.getElementById("graph-div").style.display = "none";
+    document.getElementById("yelp-result").style.display = "none";
+    document.getElementById("yelp-result-table").style.display = "none";
+}
